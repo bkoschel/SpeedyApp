10686edadf4af0c1cde47b7dd4acacfeee3b9b58
bkoschel <67294728+bkoschel@users.noreply.github.com>
2022-10-03 14:58:51 -0700

    added Run, Bike and Swim classes to represent different exercises to be logged

---
+++ src/main/model/Run.java
@@ -0,0 +1,82 @@
+package model;
+
+// Represents a Run entry having a title, date, distance, duration and pace
+public class Run {
+    private String title;
+    private String date;
+    private double distance;
+    private double duration;
+    private double pace;
+    private int elevation;
+
+    /*
+     * REQUIRES: pace, distance, duration, and elevation must be non-negative values
+     * EFFECTS: sets the title of the swimming activity to default 'New Run'; Sets the date to current
+     * date and time; sets the distance to 0.0km; sets the duration to 00(hours):00(minutes):00(seconds);
+     * sets the pace to 0:00/km; sets the elevation to 0 meters;
+     */
+    public Run() {
+        title = "New Run";
+        date = "";
+        distance = 0.0;
+        duration = 0.0;
+        pace = 0.0;
+        elevation = 0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public double getPace() {
+        return pace;
+    }
+
+    public int getElevation() {
+        return elevation;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setPace(double pace) {
+        this.pace = pace;
+    }
+
+    public void setElevation(int elevation) {
+        this.elevation = elevation;
+    }
+
+    public double calculatePace() {
+        return 0.0;
+    }
+
+    public double changeUnits() {
+        return 0.0;
+    }
+}
---
+++ src/main/model/Swim.java
@@ -0,0 +1,74 @@
+package model;
+
+// Represents a Swim entry having a title, date, distance, duration and pace
+public class Swim {
+    private String title;
+    private String date;
+    private double distance;
+    private double duration;
+    private double pace;
+
+    /*
+    * REQUIRES: pace, distance and duration must be non-negative values
+    * EFFECTS: sets the title of the swimming activity to default 'New Swim'; Sets the date to current
+    * date and time; sets the distance to 0.0km; sets the duration to 00(hours):00(minutes):00(seconds);
+    * sets the pace to 0:00/km
+    */
+    public Swim() {
+        title = "New Swim";
+        date = "";
+        distance = 0.0;
+        duration = 0.0;
+        pace = 0.0;
+    }
+
+    // getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public double getPace() {
+        return pace;
+    }
+
+    // setters
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setPace(double pace) {
+        this.pace = pace;
+    }
+
+    public double calculatePace() {
+        return 0.0;
+    }
+
+    public double changeUnits() {
+        return 0.0;
+    }
+}
