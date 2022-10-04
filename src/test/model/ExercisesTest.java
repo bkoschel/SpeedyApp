f1c49b6eb7dc5335a8a95bbe4006430267d2510d
bkoschel <67294728+bkoschel@users.noreply.github.com>
2022-10-04 10:43:06 -0700

    added ExerciseLog getter and setter tests

--- src/test/model/ExercisesTest.java
+++ src/test/model/ExercisesTest.java
@@ -5,6 +5,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ExercisesTest {
+    private Swim swim;
+    private Bike bike;
+    private Run run;
 
+    private ExerciseLog exerciseLog;
+
+    @BeforeEach
+    void setUp() {
+        swim = new Swim();
+        bike = new Bike();
+        run = new Run();
+
+        exerciseLog = new ExerciseLog();
+    }
+
+    @Test
+    public void testAddExercise() {
+        assertFalse(exerciseLog.containsExercise(swim));
+        exerciseLog.addExercise(swim);
+        assertTrue(exerciseLog.containsExercise(swim));
+    }
+
+    @Test
+    public void testContainsExercise() {
+        assertFalse(exerciseLog.containsExercise(bike));
+        assertFalse(exerciseLog.containsExercise(run));
+        exerciseLog.addExercise(bike);
+        exerciseLog.addExercise(run);
+        assertTrue(exerciseLog.containsExercise(bike));
+        assertTrue(exerciseLog.containsExercise(run));
+        assertFalse(exerciseLog.containsExercise(swim));
+    }
+
+    @Test
+    public void testRemoveExercise() {
+        exerciseLog.addExercise(swim);
+        exerciseLog.addExercise(bike);
+        assertTrue(exerciseLog.containsExercise(bike));
+        assertTrue(exerciseLog.containsExercise(swim));
+        exerciseLog.removeExercise(swim);
+        assertFalse(exerciseLog.containsExercise(swim));
+        exerciseLog.removeExercise(bike);
+        assertFalse(exerciseLog.containsExercise(bike));
+    }
 
 }
